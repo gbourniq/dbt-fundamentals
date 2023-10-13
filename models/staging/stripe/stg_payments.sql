ef66e14977e9c3fd78bd55d00ca1bce34ce7cd95
Guillaume Bournique <guillaume.bournique@sigtech.com>
2023-10-13 02:18:41 +0200

    feat: add models to ingest and aggregate stripe fact data (modelling part 2)

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,9 @@
+
+SELECT
+  orderid       AS order_id,
+  paymentmethod AS payment_method,
+  status,
+  amount / 100  AS amount,
+  created       AS created_at
+
+FROM {{ source('stripe', 'payment') }}
