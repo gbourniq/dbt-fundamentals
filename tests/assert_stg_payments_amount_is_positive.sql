ac853a643e9d3686cc4f0758cb07d3e34e0ffd6d
Guillaume Bournique <guillaume.bournique@sigtech.com>
2023-10-14 15:10:42 +0200

    test: update freshness test

--- tests/assert_stg_payments_amount_is_positive.sql
+++ tests/assert_stg_payments_amount_is_positive.sql
@@ -1,6 +1,8 @@
--- Singular test on stg_payments
+-- Singular or "specific" test (business logic oriented) on stg_payments
 -- Refunds have a negative amount, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail.
+-- Can reference multiple tables, and will be shown in the lineage graph.
+
 SELECT
   order_id,
   SUM(amount) AS total_amount
